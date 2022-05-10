8270907128a436329fef54fd895b3028674ae2a4
krishnaraj <krishnaraj3010@gmail.com>
2022-05-10 16:16:44 +0530

    Demo-1 on CRUD operations

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,7 +4,6 @@ import com.example.demo.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
